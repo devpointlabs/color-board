6a6a2297edd083aab78988e670b33959f292afcf
Nick Semerad <nicksemerad@gmail.com>
2020-04-02 18:54:45 -0600

    added likes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_005113) do
+ActiveRecord::Schema.define(version: 2020_04_03_005409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2020_04_03_005113) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["board_id"], name: "index_colors_on_board_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "board_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["board_id"], name: "index_likes_on_board_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -65,4 +74,6 @@ ActiveRecord::Schema.define(version: 2020_04_03_005113) do
 
   add_foreign_key "boards", "users"
   add_foreign_key "colors", "boards"
+  add_foreign_key "likes", "boards"
+  add_foreign_key "likes", "users"
 end
