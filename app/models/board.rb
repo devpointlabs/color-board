21d283227e3a75d7144f7f0b4dad1610d6315ada
Nick Semerad <nicksemerad@gmail.com>
2020-04-02 18:59:06 -0600

    relations

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,6 @@
 class Board < ApplicationRecord
   belongs_to :user
+  has_many :colors
+  has_many :likes
+  # has_many :liking_users, through: :likes, source: user 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
   extend Devise::Models
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+  has_many :boards
+  has_many :likes
+  # has_many :liked_boards, through: :likes, source: board 
 end
