18044ed0a1ef8ef0ffd5e154f8956720a43eed8f
maxbehling <elmachomax123@gmail.com>
2020-04-05 13:22:39 -0600

    Rename Home component export name

--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import NewBoard from './NewBoard'
 
-export default class App extends React.Component {
+export default class Home extends React.Component {
   constructor(props){
     super(props);
     this.state = {
