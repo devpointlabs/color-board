3e2446b9fd55e5d3186a9129f85242862388c1ea
Henry Doan <henrydoan96@gmail.com>
2020-04-22 23:07:31 -0600

    Update Board.js

--- client/src/components/shared/Board.js
+++ client/src/components/shared/Board.js
@@ -1,56 +1,140 @@
-import React from 'react';
-import styled from 'styled-components'
-import Liked from './Liked';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { BoardConsumer } from '../../providers/BoardProvider';
+import axios from 'axios';
 
-const Styles = styled.div`
+class Board extends Component {
+  state = { colors: [], userImg: ''}
 
-.card {
-  width: 17rem;
-  height: 18rem;
-  padding: 10px;
-  margin: 2rem;
-  word-wrap: break-word;
-},
-`
+  componentDidMount() {
+    const { id, user_id } = this.props
+    axios.get(`/api/board_colors/${id}`)
+      .then ( res => {
+        this.setState({ colors: res.data })
+      })
+      .catch( err => {
+        console.log(err);
+      })
 
-const Board = ({ title, desc }) => (
-  <Styles>
-    <div style={{
-					padding: '5rem',
-          wordWrap: 'break-word',
-			    left: '4rem',
-					width: '18rem',
-				  height: '17rem',
-          border: '1px solid lightgrey',
-					//boxShadow: '-9px 11px 20px 1px lightgrey',
-				}}>
-					<span style={{
-						position: 'relative',
-				    left: '1.5rem',
-				    top: '2rem',
-						fontWeight: 'bold',
-				    fontSize: '18px',
-				    fontFamily: 'Rubik',
-					}}>
-						
-					</span>
-					<span style={{
-						position: 'relative',
-				    left: '2.5rem',
-				    top: '9.3rem',
-				    fontSize: '18px',
-				    fontFamily: 'Rubik',
-						overflowWrap: 'break-word',
-            background: 'lightgrey',
-            padding: '3px 7px 3px 7px',
-            borderRadius: '9px',
-            color: 'white'
-					}}>
-						{desc} 
-					</span>	
-				</div>
-  </Styles>
+    axios.post(`/api/user_image`, { user_id })
+      .then ( res => {
+        this.setState({ userImg: res.data })
+      })
+      .catch( err => {
+        console.log(err);
+      })
+  }
+
+  render() {
+    const { title, desc, id } = this.props
+    return (
+      <div style={{
+        display: 'flex',
+        flexFlow: 'flex-direction',
+        marginBottom: '4rem'
+      }}>
+      <div style={{
+        width: '20rem',
+        height: '17rem',
+        border: 'none',
+        color: 'offwhite',
+        padding: '15px',
+        borderRadius: '10px 0 0 10px',
+        boxShadow: '-9px 11px 20px 1px lightgrey',
+        wordWrap: 'break-word',
+      }}>
+        <h5
+          style={{
+            color: 'black',
+            fontWeight: 'bolder'
+          }}
+        >
+          {title}
+        </h5>
+        <p
+          style={{
+            color: 'black',
+            overflowY: 'auto',
+            height: '100px',
+          }}
+        >
+          {desc}
+        </p>
+        <br />
+        <img src={require('./heart.png')}
+          style={{
+            width: '1.5rem',
+            height: 'auto',
+          }}
+        />
+        { this.state.userImg !== '' ?
+          <img src={ this.state.userImg }
+            style={{
+              width: '40px',
+              height: '40px',
+              objectFit: 'cover',
+              objectPosition: '50% 0',
+              marginLeft: '13rem',
+              borderRadius: '50%',
+            }}
+          />
+        :
+        <img src={ require('./user.png') }
+            style={{
+              width: '40px',
+              marginLeft: '13rem',
+              borderRadius: '50%',
+            }}
+          />  
+        }
+      </div>
+      { this.state.colors.map( c =>
+        <Link  
+          to={{ 
+            pathname:`/boards/${id}/colors/${c.id}`, 
+            state: {color: c} 
+          }}>
+          <div style={{ 
+              backgroundColor: `${c.hex}`, 
+              width: '20rem',
+              height: '17rem',
+              wordWrap: 'break-word',
+            }}>
+            <span style={{
+              fontWeight: 'bold',
+              fontSize: '18px',
+              fontFamily: 'Rubik',
+            }}>
+            </span>
+            <span style={{
+              position: 'relative',
+              left: '7rem',
+              top: '14.3rem',
+              fontSize: '18px',
+              fontFamily: 'Rubik',
+              overflowWrap: 'break-word',
+              background: 'lightgrey',
+              padding: '3px 7px 3px 7px',
+              borderRadius: '9px',
+              color: 'white'
+            }}>
+            {c.hex}
+            </span>	
+        </div>
+      </Link>
+      )}
+    </div>
+    )
+  }
+}
+
+const ConnectedBoard = (props) => (
+  <BoardConsumer> 
+    {
+      value =>
+      <Board {...props} {...value} />
+    }
+  </BoardConsumer>
 )
 
-
-export default Board;
+export default ConnectedBoard;
